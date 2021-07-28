6db29511df31c570a2dfc2b4bbc5d642808062bb
Jhovany <jmorales@credicapital.com.mx>
2021-07-28 12:45:46 -0500

    reorganizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BarraLateralComponent } from './sidenavbar/barra-lateral/barra-lateral.component';
-import { ClientesComponent } from './sidenavbar/clientes/clientes.component';
-import { LoginComponent } from './login/login.component';
-import { PdvComponent } from './sidenavbar/pdv/pdv.component';
-import { ProductosComponent } from './sidenavbar/productos/productos.component';
-import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
+import { BarraLateralComponent } from './components/sidenavbar/barra-lateral/barra-lateral.component';
+import { ClientesComponent } from './components/sidenavbar/clientes/clientes.component';
+import { LoginComponent } from './components/login/login.component';
+import { PdvComponent } from './components/sidenavbar/pdv/pdv.component';
+import { ProductosComponent } from './components/sidenavbar/productos/productos.component';
+import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.component';
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { MatSliderModule } from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BarraLateralComponent } from './sidenavbar/barra-lateral/barra-lateral.component';
+import { BarraLateralComponent } from './components/sidenavbar/barra-lateral/barra-lateral.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { PdvComponent } from './sidenavbar/pdv/pdv.component';
-import { ClientesComponent } from './sidenavbar/clientes/clientes.component';
-import { ProductosComponent } from './sidenavbar/productos/productos.component';
+import { PdvComponent } from './components/sidenavbar/pdv/pdv.component';
+import { ClientesComponent } from './components/sidenavbar/clientes/clientes.component';
+import { ProductosComponent } from './components/sidenavbar/productos/productos.component';
 import { FormsModule } from '@angular/forms';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { DemoMaterialModule } from './material-module';
@@ -23,7 +23,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms'; // <== add the imports!
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
+import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.component';
 @NgModule({
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   declarations: [
