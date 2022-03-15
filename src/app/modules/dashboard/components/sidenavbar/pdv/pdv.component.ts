1ab71e2c1276422749f8ea6166b746a462e6b222
andres <camr230794@gmail.com>
2022-03-15 17:14:00 -0600

    cambios

--- src/app/modules/dashboard/components/sidenavbar/pdv/pdv.component.ts
+++ src/app/modules/dashboard/components/sidenavbar/pdv/pdv.component.ts
@@ -3,19 +3,18 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { ClientinfoService } from '../../../../../service/clientinfo.service'
 import { ClienteModel, ClientesInfo } from '../../../../../Models/clientemodel'
 
-const ELEMENT_DATA: ClientesInfo[]=[]
-// = {
-//     id_cliente: 0,
-//     nombre_usuario: "",
-//     nombre_completo: "",
-//     edad: 0,
-//     fecha_registro: "",
-//     hora_registro: "",
-//     fecha_nacimiento: "",
-//     email: "",
-//     tel: "",
-//     estatus: ""
-// }
+const ELEMENT_DATA: ClientesInfo= {
+    id_cliente: 0,
+    nombre_usuario: "",
+    nombre_completo: "",
+    edad: 0,
+    fecha_registro: "",
+    hora_registro: "",
+    fecha_nacimiento: "",
+    email: "",
+    tel: "",
+    estatus: ""
+}
 
 @Component({
   selector: 'app-pdv',
@@ -32,12 +31,11 @@ export class PdvComponent implements OnInit {
   constructor(private ClienteService: ClientinfoService) { }
   nomUsuario = "";
   
-  buscarCliente(){
+  async buscarCliente(){
     this.nomUsuario = this.profileForm.get('nombreUsuario')!.value;
-    this.ClienteService.getCliente(this.nomUsuario).subscribe(data => {
-      this.dataSource = data;
-    })
-    let getid = document.getElementById("Lista") as HTMLElement; 
+    var resp = await this.ClienteService.getCliente(this.nomUsuario);
+    this.dataSource = resp;
+    let getid = document.getElementById("ProfileData") as HTMLElement; 
     getid.style.visibility = "visible";
   }
   ngOnInit(): void {
--- src/app/service/clientinfo.service.ts
+++ src/app/service/clientinfo.service.ts
@@ -37,10 +37,9 @@ export class ClientinfoService {
     return this.http.get<ClientesInfo[]>(this.url, {headers: this.headers})
   }
 
-  getCliente(Usuario: string): Observable<ClientesInfo[]> {
+  async getCliente(Usuario: string): Promise<ClientesInfo> {
     this.getToken()
     this.headers
-    return this.http.get<ClientesInfo[]>(`http://127.0.0.1:8000/clientes/getcliente?usuariocliente=${Usuario}`, {headers: this.headers})
+    return await this.http.get<ClientesInfo>(`http://127.0.0.1:8000/clientes/getcliente?usuariocliente=${Usuario}`,{headers: this.headers}).toPromise()
   }
-
 }
